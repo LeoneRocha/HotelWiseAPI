5696f9d5be9f1fbaa75e7ae63d6cc6c2add0ba96
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-12-04 23:44:13 -0300

    teste JWT e azure

--- HotelWise.API/Configure/ServiceCollectionConfigureSecurity.cs
+++ HotelWise.API/Configure/ServiceCollectionConfigureSecurity.cs
@@ -2,6 +2,7 @@
 using HotelWise.Domain.Dto;
 using HotelWise.Domain.Dto.AppConfig;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -12,17 +13,177 @@ namespace HotelWise.API.Configure
     {
         public static void Configure(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration, AzureAdConfig azureConfig)
         {
+            addSecurity(services, tokenConfigurations, configuration);
             //addSecuritySimple(services, tokenConfigurations, configuration);
-
-            //addSecurity(services, tokenConfigurations, configuration);
+            ///addSecuritySimpleJWT1(services, tokenConfigurations, configuration, azureConfig);
             //https://learn.microsoft.com/en-us/samples/azure-samples/ms-identity-ciam-javascript-tutorial/ms-identity-ciam-javascript-tutorial-2-call-api-angular/
         }
-
-        private static void addSecuritySimpleJWT(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration)
+        private static void addSecuritySimpleJWT1(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration, AzureAdConfig azureConfig)
         {
-            var adSec = configuration.GetSection("AzureAd");
+            services.AddAuthentication(options =>
+            {
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(adSec); //FUNCIONA SO COM ISSO 
+                options.DefaultScheme = "AzureAd";
+                options.DefaultAuthenticateScheme = "AzureAd";
+                options.DefaultChallengeScheme = "AzureAd";
+            })
+                .AddJwtBearer("AzureAd", options =>
+                {
+                    options.Authority = $"https://login.microsoftonline.com/{azureConfig.TenantId}";
+                    options.Audience = azureConfig.ClientId; // Ou "api://YOUR_API_ID"
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = $"https://login.microsoftonline.com/{azureConfig.TenantId}/v2.0",
+                        ValidAudience = azureConfig.ClientId // Ou "api://YOUR_API_ID"
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                            logger.Error("AzureAd Authentication failed.", context.Exception);
+                            return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                            var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                            logger.Information("AzureAd Token validated successfully.");
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AzureAd", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddAuthenticationSchemes("AzureAd");
+                });
+            });
+        }
+
+        private static void addSecuritySimpleJWT(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration, AzureAdConfig azureConfig)
+        {
+            //Funcionou
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = $"https://login.microsoftonline.com/{azureConfig.TenantId}";
+                    options.Audience = azureConfig.ClientId; // Ou "api://YOUR_API_ID"
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = $"https://login.microsoftonline.com/{azureConfig.TenantId}/v2.0",
+                        ValidAudience = azureConfig.ClientId // Ou "api://YOUR_API_ID"
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                            logger.Error("AzureAd Authentication failed.", context.Exception);
+                            return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                            var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                            logger.Information("AzureAd Token validated successfully.");
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Bearer", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                });
+            });
+        }
+
+        private static void addSecuritySimpleJWT2(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration, AzureAdConfig azureConfig)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer("Bearer", options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidIssuer = tokenConfigurations.Issuer,
+                   ValidAudience = tokenConfigurations.Audience,
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfigurations.Secret))
+               };
+               options.Events = new JwtBearerEvents
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                       logger.Error("JWT Authentication failed.", context.Exception);
+                       return Task.CompletedTask;
+                   },
+                   OnTokenValidated = context =>
+                   {
+                       var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                       logger.Information("JWT Token validated successfully.");
+                       return Task.CompletedTask;
+                   }
+               };
+           })
+           .AddJwtBearer("AzureAd", options =>
+           {
+               options.Authority = $"https://login.microsoftonline.com/{azureConfig.TenantId}";
+               options.Audience = azureConfig.ClientId; // Ou "api://YOUR_API_ID"
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidIssuer = $"https://login.microsoftonline.com/{azureConfig.TenantId}/v2.0",
+                   ValidAudience = azureConfig.ClientId // Ou "api://YOUR_API_ID"
+               };
+               options.Events = new JwtBearerEvents
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                       logger.Error("AzureAd Authentication failed.", context.Exception);
+                       return Task.CompletedTask;
+                   },
+                   OnTokenValidated = context =>
+                   {
+                       var logger = context.HttpContext.RequestServices.GetRequiredService<Serilog.ILogger>();
+                       logger.Information("AzureAd Token validated successfully.");
+                       return Task.CompletedTask;
+                   }
+               };
+           });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("BearerPolicy", policy =>
+                {
+                    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireAuthenticatedUser();
+                });
+
+                options.AddPolicy("AzureAdPolicy", policy =>
+                {
+                    policy.AddAuthenticationSchemes("AzureAd");
+                    policy.RequireAuthenticatedUser();
+                });
+            });
         }
 
         private static void addSecuritySimple(IServiceCollection services, TokenConfigurationDto tokenConfigurations, IConfiguration configuration)
